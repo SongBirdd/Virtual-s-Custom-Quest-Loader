b0e66dc97ef24b74253044ac746d1b6d8c5db245
unknown <flowlesstv@protonmail.com>
2023-12-09 22:05:01 +0100

    Add files

---
+++ src/mod.js
@@ -0,0 +1,110 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ConfigTypes_1 = require("C:/snapshot/project/obj/models/enums/ConfigTypes");
+const path = __importStar(require("path"));
+const fs = require('fs');
+const modPath = path.normalize(path.join(__dirname, '..'));
+class VCQL {
+    postDBLoad(container) {
+        const database = container.resolve("DatabaseServer").getTables();
+        const imageRouter = container.resolve("ImageRouter");
+        const logger = container.resolve("WinstonLogger");
+        const config = container.resolve("ConfigServer").getConfig(ConfigTypes_1.ConfigTypes.QUEST);
+        this.importQuests(database, logger, config);
+        this.importLocales(database);
+        this.routeImages(imageRouter, logger);
+    }
+    loadFiles(dirPath, extName, cb) {
+        if (!fs.existsSync(dirPath))
+            return;
+        const dir = fs.readdirSync(dirPath, { withFileTypes: true });
+        dir.forEach(item => {
+            const itemPath = path.normalize(`${dirPath}/${item.name}`);
+            if (item.isDirectory())
+                this.loadFiles(itemPath, extName, cb);
+            else if (extName.includes(path.extname(item.name)))
+                cb(itemPath);
+        });
+    }
+    importQuests(database, logger, config) {
+        let questCount = 0;
+        this.loadFiles(`${modPath}/database/quests/`, [".json"], function (filePath) {
+            const item = require(filePath);
+            if (Object.keys(item).length < 1)
+                return;
+            for (const quest in item) {
+                if (item[quest].side == "Usec")
+                    config.usecOnlyQuests.push(quest);
+                if (item[quest].side == "Bear")
+                    config.bearOnlyQuests.push(quest);
+                item[quest].side = "Pmc";
+                database.templates.quests[quest] = item[quest];
+                questCount++;
+            }
+        });
+        logger.success(`[VCQL] Loaded ${questCount} custom quests.`);
+    }
+    importLocales(database) {
+        const serverLocales = ['ch', 'cz', 'en', 'es', 'es-mx', 'fr', 'ge', 'hu', 'it', 'jp', 'pl', 'po', 'ru', 'sk', 'tu'];
+        const addedLocales = {};
+        for (const locale of serverLocales) {
+            this.loadFiles(`${modPath}/database/locales/${locale}`, [".json"], function (filePath) {
+                const localeFile = require(filePath);
+                if (Object.keys(localeFile).length < 1)
+                    return;
+                for (const currentItem in localeFile) {
+                    database.locales.global[locale][currentItem] = localeFile[currentItem];
+                    if (!Object.keys(addedLocales).includes(locale))
+                        addedLocales[locale] = {};
+                    addedLocales[locale][currentItem] = localeFile[currentItem];
+                }
+            });
+        }
+        // placeholders
+        for (const locale of serverLocales) {
+            if (locale == "en")
+                continue;
+            for (const englishItem in addedLocales["en"]) {
+                if (locale in addedLocales) {
+                    if (englishItem in addedLocales[locale])
+                        continue;
+                }
+                if (database.locales.global[locale] != undefined)
+                    database.locales.global[locale][englishItem] = addedLocales["en"][englishItem];
+            }
+        }
+    }
+    routeImages(imageRouter, logger) {
+        let imageCount = 0;
+        this.loadFiles(`${modPath}/res/quests/`, [".png", ".jpg"], function (filePath) {
+            imageRouter.addRoute(`/files/quest/icon/${path.basename(filePath, path.extname(filePath))}`, filePath);
+            imageCount++;
+        });
+        logger.success(`[VCQL] Loaded ${imageCount} custom images.`);
+    }
+}
+module.exports = { mod: new VCQL() };
+//# sourceMappingURL=mod.js.map
